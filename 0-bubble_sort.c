5b76296271a88e2b77ca76192a05722e8e440571
NMenendez24 <nahuelmt24@gmail.com>
2023-01-05 05:59:08 -0800

    Task-0-Compilation-Fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,13 +1,20 @@
 #include "sort.h"
 
+/**
+  * bubble_sort - Sorts an array of integers
+  * @size: Size of the array
+  * @array: Array to sort
+  */
+
 void bubble_sort(int *array, size_t size)
 {
-	int i = 0, check = 0, j = 0;
+	size_t i = 0, check = 0;
+	int j = 0;
 
-	while(1)
+	while (1)
 	{
 		check = 0;
-		for(i = 0; i < size - 1; i++)
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] <= array[i + 1])
 				check++;
