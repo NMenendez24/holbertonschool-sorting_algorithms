d119120a9bef690c76347cce5fb1a44f19479be4
Repli1 <5724@holbertonstudents.com>
2023-01-06 08:49:42 -0800

    Task 1 fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,44 +6,44 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	int i, check;
+	int i;
 	listint_t *head = *list, *head2, *buf, *dup = head;
 
 	while (head)
 	{
 		dup = head;
-		check = 0;
-		head2 = head;
-		buf = head;
 		i = head->n;
+		head2 = head;
+		head = head->next;
 		while (head2)
 		{
+			buf = head2;
 			if (i < head2->n)
 			{
-				check = 1;
-				buf = head2;
+				while (buf && (buf->n != i))
+				{
+					if (dup->prev)
+						(dup->prev)->next = dup->next;
+					if (dup->next)
+						(dup->next)->prev = dup->prev;
+					dup->next = buf;
+					if (buf->prev)
+					{
+						dup->prev = buf->prev;
+						(buf->prev)->next = dup;
+					}
+					buf->prev = dup;
+					if ((*list)->prev)
+					{
+						*list = (*list)->prev;
+						(*list)->prev = NULL;
+					}
+					if (buf->prev)
+						buf = buf->prev;
+					print_list(*list);
+				}
 			}
 			head2 = head2->prev;
 		}
-		head = head->next;
-		if (check == 0)
-			continue;
-		if (dup->prev)
-			(dup->prev)->next = dup->next;
-		if (dup->next)
-			(dup->next)->prev = dup->prev;
-		dup->next = buf;
-		if (buf->prev)
-		{
-			dup->prev = buf->prev;
-			(buf->prev)->next = dup;
-		}
-		buf->prev = dup;
-		if ((*list)->prev)
-		{
-			*list = (*list)->prev;
-			(*list)->prev = NULL;
-		}
-		print_list(*list);
 	}
 }
